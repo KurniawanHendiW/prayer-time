71d788fb1adfccc1043ffd6ae06f0ae5edd6d79a
KurniawanHendiW <kurniawanhendiwijaya@gmail.com>
2022-01-26 21:09:26 +0700

    remove log

--- views/assets/js/landingpage.js
+++ views/assets/js/landingpage.js
@@ -93,7 +93,6 @@ $(document).ready(function() {
   });
 
   $("#get_data").click(function(){
-    console.log("HAHAHAHAHAHAH")
     
     var form = document.querySelector('#prayerForm');
     var formData = new FormData(form);
@@ -120,7 +119,6 @@ $(document).ready(function() {
           $("#message").html(data.responseJSON.message);
           $(".url-group").css("display", "none");
           $(".result-group").css("display", "block");
-          console.log(status);
         });
   });
 
